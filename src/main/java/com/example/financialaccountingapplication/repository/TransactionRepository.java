76cc379ada9be718e6ff717c339601211a9a87c2
weistapki <weistapki@gmail.com>
2023-09-18 19:12:52 +0200

    added Repository

--- src/main/java/com/example/financialaccountingapplication/repository/TransactionRepository.java
+++ src/main/java/com/example/financialaccountingapplication/repository/TransactionRepository.java
@@ -1,4 +1,11 @@
 package com.example.financialaccountingapplication.repository;
 
-public class TransactionRepository {
+import com.example.financialaccountingapplication.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    // Дополнительные методы могут быть добавлены сюда, если требуется выполнить
+    // пользовательские операции для сущности Transaction.
 }
