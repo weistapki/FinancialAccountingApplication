ba9492bd0d26c54707c696d6d38c41dd33cf3e20
weistapki <weistapki@gmail.com>
2023-09-20 17:17:41 +0200

    added service

---
+++ src/main/java/com/example/financialaccountingapplication/service/TransactionService.java
@@ -0,0 +1,38 @@
+package com.example.financialaccountingapplication.service;
+
+import com.example.financialaccountingapplication.model.entity.Transaction;
+import com.example.financialaccountingapplication.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Optional<Transaction> getTransactionById(Long id) {
+        return transactionRepository.findById(id);
+    }
+
+    public Transaction saveTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public void deleteTransaction(Long id) {
+        transactionRepository.deleteById(id);
+    }
+}
+
