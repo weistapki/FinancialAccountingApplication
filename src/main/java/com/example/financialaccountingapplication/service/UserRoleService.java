10d6e47d0aa890c8878fe25fe600606443dc289b
weistapki <weistapki@gmail.com>
2023-09-23 15:23:41 +0200

    highlighting service UserRoleService and their UserRoleServiceImpl

--- src/main/java/com/example/financialaccountingapplication/service/UserRoleService.java
+++ src/main/java/com/example/financialaccountingapplication/service/UserRoleService.java
@@ -1,38 +1,14 @@
 package com.example.financialaccountingapplication.service;
 
 import com.example.financialaccountingapplication.model.entity.UserRole;
-import com.example.financialaccountingapplication.repository.UserRoleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-
-@Service
-public class UserRoleService {
-
-    private final UserRoleRepository userRoleRepository;
-
-    @Autowired
-    public UserRoleService(UserRoleRepository userRoleRepository) {
-        this.userRoleRepository = userRoleRepository;
-    }
-
-    public List<UserRole> getAllUserRoles() {
-        return userRoleRepository.findAll();
-    }
-
-    public Optional<UserRole> getUserRoleById(Long id) {
-        return userRoleRepository.findById(id);
-    }
-
-    public UserRole saveUserRole(UserRole userRole) {
-        return userRoleRepository.save(userRole);
-    }
-
-    public void deleteUserRole(Long id) {
-        userRoleRepository.deleteById(id);
-    }
+public interface UserRoleService {
+    List<UserRole> getAllUserRoles();
+    Optional<UserRole> getUserRoleById(Long id);
+    UserRole saveUserRole(UserRole userRole);
+    void deleteUserRole(Long id);
 }
 
