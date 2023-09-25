b10c648e269119769bbf8ae6cd6e358acbaa0527
weistapki <weistapki@gmail.com>
2023-09-25 18:23:10 +0200

    highlighting service UserService and their UserServiceImpl

--- src/main/java/com/example/financialaccountingapplication/repository/UserRoleRepository.java
+++ src/main/java/com/example/financialaccountingapplication/repository/UserRoleRepository.java
@@ -4,4 +4,6 @@ import com.example.financialaccountingapplication.model.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface  UserRoleRepository extends JpaRepository<UserRole,Long> {
+    // Дополнительные методы могут быть добавлены сюда, если требуется выполнить
+    // пользовательские операции для сущности UserRole.
 }
